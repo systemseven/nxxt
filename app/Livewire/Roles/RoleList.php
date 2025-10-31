0ffca33d4df444263ceba1a384ec4ba38277870d
= <=>
2025-10-31 09:30:57 -0400

    feat: ui for listing edit/delete links

--- app/Livewire/Roles/RoleList.php
+++ app/Livewire/Roles/RoleList.php
@@ -18,8 +18,8 @@ class RoleList extends Component
 
     public function removeRole($role_id)
     {
-        //delete from the role table
-        //delete from the permission_role table
+        // delete from the role table
+        // delete from the permission_role table
         dd('its morphin time...err deleting time...');
     }
 }
