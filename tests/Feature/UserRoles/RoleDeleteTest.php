d4e87acfc7f17c105544ba244bbb96c901ba2b62
= <=>
2025-10-31 10:41:24 -0400

    test: delete role test

--- tests/Feature/UserRoles/RoleDeleteTest.php
+++ tests/Feature/UserRoles/RoleDeleteTest.php
@@ -1,6 +1,16 @@
 <?php
 
+use App\Livewire\Roles\RoleList;
+use App\Models\User;
+use Spatie\Permission\Models\Role;
+
 test('user role can be deleted', function () {
-    // deletes the role
-    // check no users assigned to the role
-})->skip();
+    $role = Role::create(['name' => 'test']);
+    $user = User::factory()->create()->assignRole($role);
+    $this->actingAs($user);
+    Livewire::test(RoleList::class)
+        ->call('removeRole', $role->id);
+
+    $this->assertDatabaseMissing('roles', ['id' => $role->id]);
+    $this->assertDatabaseMissing('permissions', ['role_id' => $role->id]);
+});
